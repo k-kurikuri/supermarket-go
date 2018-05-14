822d4f5ef2b113cafb99e7df807a5be8952ff620
k-kurikuri <kurimixgoo@gmail.com>
2018-05-14 18:52:51 +0900

    add : Cookieにuidをセットする処理を実装

--- server.go
+++ server.go
@@ -5,8 +5,10 @@ import (
 	"net/http"
 
 	"io"
+	"time"
 
 	"github.com/labstack/echo"
+	"github.com/rs/xid"
 )
 
 type TemplateRender struct {
@@ -23,6 +25,10 @@ func (t *TemplateRender) Render(w io.Writer, name string, data interface{}, c ec
 
 var e *echo.Echo
 
+const (
+	userCookieName = "_uid"
+)
+
 func init() {
 	e = echo.New()
 
@@ -33,15 +39,45 @@ func init() {
 
 func main() {
 	e.GET("/", func(context echo.Context) error {
+		if _, err := getUidCookie(context); err != nil {
+			setUidCookie(context)
+		} else {
+			//todo: cookie.Value
+		}
+
 		return context.Render(http.StatusOK, "index.html", map[string]interface{}{})
 	})
 
 	e.Logger.Fatal(e.Start(":8000"))
 }
 
+// parse rendering files
 func setRenderer() {
 	renderer := &TemplateRender{
 		templates: template.Must(template.ParseGlob("views/*.html")),
 	}
 	e.Renderer = renderer
+}
+
+// Cookieにuidを書き込む
+func setUidCookie(c echo.Context) {
+	cookie := new(http.Cookie)
+	cookie.Name = userCookieName
+	cookie.Value = createUuid()
+	cookie.Expires = time.Now().Add(2 * time.Hour)
+	c.SetCookie(cookie)
+}
+
+// uidキーのCookieを取得する
+func getUidCookie(c echo.Context) (*http.Cookie, error) {
+	cookie, err := c.Cookie(userCookieName)
+
+	return cookie, err
+}
+
+// uuidを生成する
+func createUuid() string {
+	uid := xid.New()
+
+	return uid.String()
 }
