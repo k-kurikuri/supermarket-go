28fcb6d9756c89c7bd1734694df539bf98a236be
k-kurikuri <kurimixgoo@gmail.com>
2018-07-18 10:41:54 +0900

    change mongoへのhost/port情報を環境変数からも読み込めるよう修正

--- app/mongo.go
+++ app/mongo.go
@@ -1,8 +1,10 @@
 package app
 
-import "gopkg.in/mgo.v2"
+import (
+	"os"
 
-const dial = "127.0.0.1:27017"
+	"gopkg.in/mgo.v2"
+)
 
 // DBName Mongo Database Name
 const DBName = "supermarket-go"
@@ -12,10 +14,19 @@ const Table = "todolist"
 
 // GetSession get mongodb session
 func GetSession() *mgo.Session {
-	session, err := mgo.Dial(dial)
+	session, err := mgo.Dial(GetDial())
 	if err != nil {
 		panic(err)
 	}
 
 	return session
+}
+
+func GetDial() string {
+	dial := os.Getenv("MONGO_DIAL")
+	if dial == "" {
+		dial = "127.0.0.1:27017"
+	}
+
+	return dial
 }
